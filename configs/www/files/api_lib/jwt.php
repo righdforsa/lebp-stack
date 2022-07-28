a57ab09cb728f757e5c1fddee6aefb7880d7d690
Christopher Peters <righdforsa@gmail.com>
2022-07-28 18:12:43 +0000

    actually return the auth token

--- configs/www/files/api_lib/jwt.php
+++ configs/www/files/api_lib/jwt.php
@@ -2,10 +2,13 @@
 
 function newJWT($username) {
 	// TODO: Actually make this do something
+	syslog(LOG_INFO, "newJWT(): issuing new JWT for $username");
         $jwt = hash('sha256', time() . $username . "asdfasdf");
+        return $jwt;
 }
 
 function validateJWT($username) {
         // TODO: Actually make this do something
+	syslog(LOG_INFO, "validateJWT(): evaluating JWT for $username");
         return;
 }
